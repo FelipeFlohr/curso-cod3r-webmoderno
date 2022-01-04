69964d8dd61dd43181bff4ec43828c4f2eebd862
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2022-01-04 19:55:59 -0300

    Módulo 40

---
+++ 03 Iniciando com Programação/040 Cuidados com Number/cuidadosNumber.js
@@ -0,0 +1,11 @@
+// Diferente de outras línguas de programação. No JavaScript é possí-
+// vel realizar operações matemáticas com Strings
+console.log("10" / 2) // Retornará 5
+
+// Em algumas linguagens (Python se eu não me engano) a frase abaixo
+// se repetiria, no JS não
+console.log("Show!" * 2)
+
+// Problemas com ponto flutuante podem ocorrer também, porém, isso
+// não é só comum no JS
+console.log(0.1 + 0.7)
