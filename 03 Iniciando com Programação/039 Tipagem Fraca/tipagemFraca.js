afbe7247cd5133bf2d0be828c742c9f143e6b13e
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2022-01-04 19:50:08 -0300

    Módulo 39

---
+++ 03 Iniciando com Programação/039 Tipagem Fraca/tipagemFraca.js
@@ -0,0 +1,13 @@
+// JavaScript é uma linguagem fracamente tipada (parecida com Python
+// e diferente de Java) na qual os tipos não são explicitamente de-
+// clarados
+const string = "Aqui há uma string"
+const num1 = 3
+const num2 = 3.14
+const booleano = false
+
+console.log(typeof string);
+console.log(typeof num1)
+console.log(typeof num2)
+console.log(typeof booleano)
+console.log(typeof Number)
