dae219672cf30cd28c627ab9aef87b90c67f28f2
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2022-01-05 21:51:55 -0300

    Módulo 96

--- 06 Javascript - Função/095, 096 e 097 Funções Arrow/arrowFunctions.js
+++ 06 Javascript - Função/095, 096 e 097 Funções Arrow/arrowFunctions.js
@@ -18,3 +18,21 @@ console.log(dobro(2))
 // Função arrow sem parâmetro
 let ola = () => "Olá"
 console.log(ola())
+
+// "this" e Funções Arrow
+// Repare que não há a necessidade de "bindar" o this abaixo, pois
+// o this dentro de uma função arrow é léxico, isto é, se 
+function Pessoa() {
+    this.idade = 0
+
+    let intervalo = setInterval(() => {
+        this.idade++
+        console.log("Utilizando this dentro de uma função Arrow: " + this.idade)
+
+        if (this.idade >= 5) {
+            clearInterval(intervalo)
+        }
+    }, 1000)
+}
+
+new Pessoa
