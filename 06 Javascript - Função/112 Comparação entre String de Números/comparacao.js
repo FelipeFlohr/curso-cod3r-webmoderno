3a3653b1f21b8354ae8babd3090c04e2e2d16ad5
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2022-01-06 20:17:34 -0300

    Módulo 112

---
+++ 06 Javascript - Função/112 Comparação entre String de Números/comparacao.js
@@ -0,0 +1,5 @@
+// Comparação de string de números
+
+console.log('1' > '2') // Retorna false
+console.log('2' > '1') // Retorna true
+console.log('2' > '19') // Retorna true. Isso acontece pois em comparação de string de números 
