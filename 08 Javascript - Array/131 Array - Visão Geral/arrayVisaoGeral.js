841e52a90fc271ce78e9b6ccb71bbdd03c3e65fd
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2022-01-07 13:33:22 -0300

    Módulo 131

---
+++ 08 Javascript - Array/131 Array - Visão Geral/arrayVisaoGeral.js
@@ -0,0 +1,6 @@
+const array = [ 1, 2, 3, 4, 5]
+console.log("Tamanho do array: " + array.length)
+console.log("Item de índice 4: " + array[4])
+console.log("Item de índice 9: " + array[9])
+// O fato de um Array ser constante não significa que os dados
+// daquele array serão freezados
