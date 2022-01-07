677f175dba8446c81d2e8e05f9207b9bf9824ff4
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2022-01-07 03:21:03 -0300

    Módulo 129 e 130

---
+++ 07 Javascript - Objeto/129 e 130 Classe/classe.js
@@ -0,0 +1,51 @@
+// No ES2015 foi introduzido o conceito de classes para o JavaScript
+class Lancamento {
+    constructor(nome = "Genérico", valor = 0) {
+        this.nome = nome
+        this.valor = valor
+    }
+}
+
+class CicloFinanceiro {
+    constructor(mes, ano) {
+        this.mes = mes
+        this.ano = ano
+        this.lancamentos = []
+    }
+
+    addLancamentos(...lancamentos) {
+        lancamentos.forEach(l => this.lancamentos.push(l))
+    }
+
+    sumario() {
+        let valorConsolidado = 0
+        this.lancamentos.forEach(l => {
+            valorConsolidado += l.valor
+        })
+
+        return valorConsolidado
+    }
+}
+
+const salario = new Lancamento("Salário", 4500)
+const contaDeLuz = new Lancamento("Conta de Luz", -220)
+
+const contas = new CicloFinanceiro(6, 2018)
+contas.addLancamentos(salario, contaDeLuz)
+console.log(contas.sumario())
+
+// Passando herança em classes
+class Avo {
+    constructor(sobrenome) {
+        this.sobrenome = sobrenome
+    }
+}
+
+class Pai extends Avo {
+    constructor(sobrenome, profissao = "Professor") {
+        super(sobrenome)
+        this.profissao = profissao
+    }
+}
+
+console.log(new Pai("Flohr"))
