4a0f173e353e44d8a1fee5fd7f85e7327b8e8ce7
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2022-01-08 14:05:53 -0300

    Módulo 153

---
+++ 10 Node/153 Sistema de Módulos - Exports/exports.js
@@ -0,0 +1,3 @@
+this.a = 1 // Exportando atribuindo um valor ao "this"
+exports.b = 2 // Exportando atribuindo um valor ao "exports"
+module.exports.c = 3 // Exportando atribuindo um valor ao "module.exports"
