5cfa3b67f56b99c5d1fdb0b21c5284528feb085c
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2022-01-12 20:27:17 -0300

    Módulo 225

--- 13 HTML/223, 224 e 225 Formulário/server.js
+++ 13 HTML/223, 224 e 225 Formulário/server.js
@@ -6,7 +6,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post("/usuarios", (req, res) => {
     console.log(req.body)
-    res.send("<h1>Parabéns</h1>")
+    res.send("<h1>Parabéns. Usuário incluído</h1>")
+})
+
+app.post("/usuarios/3", (req, res) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    res.send("<h1>Parabéns. Usuário Alterado!</h1>")
 })
 
 app.listen(3003, () => {
