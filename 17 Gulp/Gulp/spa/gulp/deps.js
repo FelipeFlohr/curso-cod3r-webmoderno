0e4a25f8ef37539a50055685479ad8edc07f3fee
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2022-02-28 17:15:12 -0300

    Módulo 326

--- 17 Gulp/Gulp/spa/gulp/deps.js
+++ 17 Gulp/Gulp/spa/gulp/deps.js
@@ -1,11 +1,17 @@
 const gulp = require("gulp")
+const uglifycss = require("gulp-uglifycss")
+const concat = require("gulp-concat")
 
 function depsCSS(callback) {
-    return callback()
+    return gulp.src("node_modules/font-awesome/css/font-awesome.css")
+        .pipe(uglifycss({ "uglyComments": false })) // Não irá remover os comentários pois há licença
+        .pipe(concat("deps.min.css"))
+        .pipe(gulp.dest("build/assets/css"))
 }
 
 function depsFonts(callback) {
-    return callback()
+    return gulp.src("node_modules/font-awesome/fonts/*.*") // Irá pegar todas as fontes dentro da pasta
+        .pipe(gulp.dest("build/assets/fonts"))
 }
 
 module.exports = {
