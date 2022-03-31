3a10ec5e221dd23f3962290a8e48626185637a2a
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2022-03-31 01:59:17 -0300

    Módulo 398

---
+++ 22 React/398 Primeiro Componente/exercicios/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Primeiro from "./components/Primeiro";
+
+// Renderizando o componente
+ReactDOM.render(<Primeiro/>, document.getElementById("root"))
