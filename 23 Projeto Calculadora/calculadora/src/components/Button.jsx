733bfdb5501d5867bc9b10a04bf488687ace005e
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2022-04-03 12:44:35 -0300

    Módulo 409

--- 23 Projeto Calculadora/calculadora/src/components/Button.jsx
+++ 23 Projeto Calculadora/calculadora/src/components/Button.jsx
@@ -2,4 +2,13 @@ import React from "react"
 import "./Button.css"
 
 export default props =>
-    <button className="button">{props.label}</button>
+    <button className={
+        `button
+        ${props.operation ? "operation": ""}
+        ${props.double ? "double" : ""}
+        ${props.triple ? "triple" : ""}`
+    }
+    onClick={e => props.click(props.label)}
+    >
+        {props.label}
+    </button>
