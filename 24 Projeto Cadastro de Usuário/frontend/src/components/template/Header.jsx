ac16e473f7e322d2293916216433cfb8d7099e6d
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2022-04-05 13:38:57 -0300

    Módulo 418

--- 24 Projeto Cadastro de Usuário/frontend/src/components/template/Header.jsx
+++ 24 Projeto Cadastro de Usuário/frontend/src/components/template/Header.jsx
@@ -2,6 +2,9 @@ import "./Header.css"
 import React from "react"
 
 export default props =>
-    <header className="header">
-        Cabeçalho
+    <header className="header d-none d-sm-flex flex-column"> {/* As classes "d-none" e "d-sm-flex" fazem com que o Header não apareça em dispositivos móveis (responsividade) */}
+        <h1 className="mt-3">
+            <i className={`fa fa-${props.icon}`}></i> {props.title}
+        </h1>
+        <p className="lead text-muted">{props.subtitle}</p>
     </header>
