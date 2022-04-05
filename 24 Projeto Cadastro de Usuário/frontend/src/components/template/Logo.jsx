c2bd44725b22f8f95abefde271870f6fbdf7e5cd
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2022-04-05 13:15:19 -0300

    Módulo 417

--- 24 Projeto Cadastro de Usuário/frontend/src/components/template/Logo.jsx
+++ 24 Projeto Cadastro de Usuário/frontend/src/components/template/Logo.jsx
@@ -1,7 +1,10 @@
 import "./Logo.css"
+import logo from "../../assets/img/logo.png"
 import React from "react"
 
 export default props =>
     <aside className="logo">
-        
+        <a href="/" className="logo">
+            <img src={logo} alt="logo" />
+        </a>
     </aside>
