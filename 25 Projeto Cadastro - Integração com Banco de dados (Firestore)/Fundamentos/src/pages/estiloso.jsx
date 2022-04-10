607719fa0470d76f4eceaa27782ae63dbcda96cd
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2022-04-10 19:36:23 -0300

    Módulo 440

--- 25 Projeto Cadastro - Integração com Banco de dados (Firestore)/Fundamentos/src/pages/estiloso.jsx
+++ 25 Projeto Cadastro - Integração com Banco de dados (Firestore)/Fundamentos/src/pages/estiloso.jsx
@@ -1,9 +1,12 @@
+import Layout from "../components/Layout"
 import styles from "./Estiloso.module.css"
 
 export default function Estiloso() {
     return (
-        <div className={styles.roxo}>
-            <h1>Estilo usando Módulos CSS</h1>
-        </div>
+        <Layout titulo="Exemplo de CSS Modularizado">
+            <div className={styles.roxo}>
+                <h1>Estilo usando Módulos CSS</h1>
+            </div>
+        </Layout>
     )
 }
