8b3c15f2e182859529b0489e9726e4a48776a272
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2022-04-12 16:29:27 -0300

    Módulo 453

---
+++ 25 Projeto Cadastro - Integração com Banco de dados (Firestore)/Cadastro/src/pages/index.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+        <div className="flex 
+        h-screen
+        justify-center
+        items-center
+        bg-gradient-to-r
+        from-purple-500
+        to-blue-600">
+            <span className="text-4xl">Texto</span>
+        </div>
+    )
+}
