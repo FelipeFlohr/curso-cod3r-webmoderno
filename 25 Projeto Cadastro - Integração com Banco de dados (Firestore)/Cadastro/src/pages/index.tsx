8017f23542bca2bab88146f1262613ccb755ff19
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2022-04-12 17:06:53 -0300

    Módulo 454

--- 25 Projeto Cadastro - Integração com Banco de dados (Firestore)/Cadastro/src/pages/index.tsx
+++ 25 Projeto Cadastro - Integração com Banco de dados (Firestore)/Cadastro/src/pages/index.tsx
@@ -1,13 +1,18 @@
+import Layout from "../components/Layout";
+
 export default function Home() {
     return (
-        <div className="flex 
-        h-screen
+        <div className="flex
         justify-center
         items-center
+        h-screen
         bg-gradient-to-r
-        from-purple-500
-        to-blue-600">
-            <span className="text-4xl">Texto</span>
+        from-blue-500
+        to-purple-500
+        text-white">
+            <Layout titulo="Cadastro Simples">
+                <span>Conteúdo</span>
+            </Layout>
         </div>
     )
 }
