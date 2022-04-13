c9ea03c8fe94a4b05a40a6560951ebbc18692808
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2022-04-13 10:39:45 -0300

    Módulo 466

--- 25 Projeto Cadastro - Integração com Banco de dados (Firestore)/Cadastro/src/pages/index.tsx
+++ 25 Projeto Cadastro - Integração com Banco de dados (Firestore)/Cadastro/src/pages/index.tsx
@@ -1,28 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Botao from "../components/Botao";
 import Formulario from "../components/Formulario";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
+import ClienteRepositorio from "../core/ClienteRepositorio";
+import ColecaoCliente from "../firebase/db/ColecaoCliente";
 
 export default function Home() {
 
+    const repo: ClienteRepositorio = new ColecaoCliente()
+
     const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
+    const [clientes, setClientes] = useState<Cliente[]>([])
     const [visivel, setVisivel] = useState<"tabela" | "form">("tabela")
 
-    const clientes = [
-        new Cliente("Ana", 34, "1"),
-        new Cliente("Bia", 35, "2"),
-        new Cliente("Cia", 36, "3"),
-        new Cliente("Dia", 37, "4")
-    ]
+    useEffect(obterTodos, []) // Será chamado assim que o elemento for inicializado
+
+    function obterTodos() {
+        repo.obterTodos().then(clientes => {
+            setClientes(clientes)
+            setVisivel("tabela")
+        })
+    }
 
     function clienteSelecionado(cliente: Cliente) {
         setCliente(cliente)
+        setVisivel("form")
     }
 
-    function clienteExcluido(cliente: Cliente) {
-        console.log(cliente.nome)
+    async function clienteExcluido(cliente: Cliente) {
+        await repo.excluir(cliente)
+        obterTodos()
     }
 
     function novoCliente() {
@@ -30,9 +39,9 @@ export default function Home() {
         setVisivel("form")
     }
 
-    function salvarCliente(cliente: Cliente) {
-        console.log(cliente)
-        setVisivel("tabela")
+    async function salvarCliente(cliente: Cliente) {
+        await repo.salvar(cliente)
+        obterTodos()
     }
 
     return (
