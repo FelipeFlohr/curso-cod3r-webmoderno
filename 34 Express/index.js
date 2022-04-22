8cf5336e6bcefcea10a9d1ad94f4beede32157e2
= <felipeflohrlol@gmail.com>
2022-04-22 02:05:22 -0300

    Módulo 602

--- 34 Express/index.js
+++ 34 Express/index.js
@@ -5,3 +5,12 @@ const app = express()
 app.listen(3000, () => {
     console.log("Aplicação rondando na porta 3000.")
 })
+
+// Com o "use", qualquer requisição HTTP retornará "Bom dia!"
+app.use("/opa", (req, res) => {
+    res.send("Bom dia!")
+})
+
+app.get("/mensagem", (req, res) => {
+    res.send("Mensagem de bom dia!")
+})
