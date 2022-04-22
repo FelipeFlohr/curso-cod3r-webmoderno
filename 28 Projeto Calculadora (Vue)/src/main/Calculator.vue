87bcc53600bb6743298f326cd13c8209014f311e
= <felipeflohrlol@gmail.com>
2022-04-22 16:26:01 -0300

    Módulo 486

--- 28 Projeto Calculadora (Vue)/src/main/Calculator.vue
+++ 28 Projeto Calculadora (Vue)/src/main/Calculator.vue
@@ -1,7 +1,23 @@
 <template>
     <div class="calculator">
         <Display />
-        <Button />
+        <Button label="AC" triple />
+        <Button label="/" operation />
+        <Button label="7" />
+        <Button label="8" />
+        <Button label="9" />
+        <Button label="*" operation />
+        <Button label="4" />
+        <Button label="5" />
+        <Button label="6" />
+        <Button label="-" operation />
+        <Button label="1" />
+        <Button label="2" />
+        <Button label="3" />
+        <Button label="+" operation />
+        <Button label="0" double />
+        <Button label="." />
+        <Button label="=" />
     </div>
 </template>
 
@@ -17,11 +33,11 @@ export default {
 <style>
     .calculator {
         height: 320px;
-        widows: 235px;
+        width: 235px;
         border-radius: 5px;
         overflow: hidden;
-
         display: grid;
+
         grid-template-columns: repeat(4, 25%);
         grid-template-rows: 1fr 48px 48px 48px 48px 48px;
     }
