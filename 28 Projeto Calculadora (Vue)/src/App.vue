8265adb1820ec6764cd1a141672820a36a9a070f
= <felipeflohrlol@gmail.com>
2022-04-22 15:57:42 -0300

    Módulo 485

--- 28 Projeto Calculadora (Vue)/src/App.vue
+++ 28 Projeto Calculadora (Vue)/src/App.vue
@@ -1,12 +1,15 @@
 <template>
     <div id="app">
         <h1>Olá, Vue!</h1>
+        <Calculator />
     </div>
 </template>
 
 <script>
-export default {
+import Calculator from "./main/Calculator.vue"
 
+export default {
+    components: { Calculator }
 }
 </script>
 
